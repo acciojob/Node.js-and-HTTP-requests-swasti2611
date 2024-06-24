a5658dce521b8f0938accd28e788ceb684be3359
Swati jagtap <swatijagtap5060@gmail.com>
2024-06-24 12:41:10 +0530

    submit-1

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
 const https = require('https');
-const axios = require('axios');
 
 let server = https.createServer((req, res) => {
     if (req.url === '/todo') {
         if (req.method === 'GET') {
-            axios.get('https://jsonplaceholder.typicode.com/todos/1')
-                .then(response => {
-                    res.writeHead(200, { 'Content-Type': 'text/plain' });
-                    res.end(response.data.title);
-                })
-                .catch(err => {
-                    res.writeHead(500, { 'Content-Type': 'text/plain' });
-                    res.end('Error fetching data');
+            https.get('https://jsonplaceholder.typicode.com/todos/1', (response) => {
+                let data = '';
+
+                // A chunk of data has been received.
+                response.on('data', (chunk) => {
+                    data += chunk;
                 });
+
+                // The whole response has been received.
+                response.on('end', () => {
+                    try {
+                        const todo = JSON.parse(data);
+                        res.writeHead(200, { 'Content-Type': 'text/plain' });
+                        res.end(todo.title);
+                    } catch (err) {
+                        res.writeHead(500, { 'Content-Type': 'text/plain' });
+                        res.end('Error parsing JSON');
+                    }
+                });
+            }).on('error', (err) => {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error fetching data');
+            });
         } else {
             res.writeHead(405, { 'Content-Type': 'text/plain' });
             res.end('Method Not Allowed');
