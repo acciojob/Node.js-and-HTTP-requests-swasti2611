d3d7f11babef13c138987d57e396da0c27c4d0d2
Swati jagtap <swatijagtap5060@gmail.com>
2024-06-24 12:49:58 +0530

    submit-3

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const https = require('https');
-const http = require('http'); // Use `http` for creating the server
 
 const getTodoTitle = () => {
   return new Promise((resolve, reject) => {
@@ -17,12 +16,8 @@ const getTodoTitle = () => {
       });
 
       response.on('end', () => {
-        try {
-          const actualTitle = JSON.parse(data).title;
-          resolve(actualTitle);
-        } catch (err) {
-          reject(`Error parsing JSON: ${err}`);
-        }
+        const actualTitle = JSON.parse(data).title;
+        resolve(actualTitle);
       });
     });
 
@@ -44,30 +39,3 @@ if (require.main === module) {
 } else {
   module.exports = getTodoTitle;
 }
-
-// Create HTTP server using `http` module
-let server = http.createServer((req, res) => {
-  if (req.url === '/todo') {
-    if (req.method === 'GET') {
-      getTodoTitle()
-        .then(title => {
-          res.writeHead(200, { 'Content-Type': 'text/plain' });
-          res.end(title);
-        })
-        .catch(err => {
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('Error fetching data');
-        });
-    } else {
-      res.writeHead(405, { 'Content-Type': 'text/plain' });
-      res.end('Method Not Allowed');
-    }
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404 Not Found');
-  }
-});
-
-server.listen(3000, () => {
-  console.log('Server connected');
-});
