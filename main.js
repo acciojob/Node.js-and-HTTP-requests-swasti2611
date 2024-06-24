e0e31b34a8edc9f94070369432bd342443c86b4d
Swati jagtap <swatijagtap5060@gmail.com>
2024-06-24 12:48:36 +0530

    submit-2

--- main.js
+++ main.js
@@ -1,41 +1,73 @@
 const https = require('https');
+const http = require('http'); // Use `http` for creating the server
 
-let server = https.createServer((req, res) => {
-    if (req.url === '/todo') {
-        if (req.method === 'GET') {
-            https.get('https://jsonplaceholder.typicode.com/todos/1', (response) => {
-                let data = '';
+const getTodoTitle = () => {
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: 'jsonplaceholder.typicode.com',
+      path: '/todos/1',
+      method: 'GET'
+    };
 
-                // A chunk of data has been received.
-                response.on('data', (chunk) => {
-                    data += chunk;
-                });
+    const request = https.request(options, (response) => {
+      let data = '';
 
-                // The whole response has been received.
-                response.on('end', () => {
-                    try {
-                        const todo = JSON.parse(data);
-                        res.writeHead(200, { 'Content-Type': 'text/plain' });
-                        res.end(todo.title);
-                    } catch (err) {
-                        res.writeHead(500, { 'Content-Type': 'text/plain' });
-                        res.end('Error parsing JSON');
-                    }
-                });
-            }).on('error', (err) => {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Error fetching data');
-            });
-        } else {
-            res.writeHead(405, { 'Content-Type': 'text/plain' });
-            res.end('Method Not Allowed');
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      response.on('end', () => {
+        try {
+          const actualTitle = JSON.parse(data).title;
+          resolve(actualTitle);
+        } catch (err) {
+          reject(`Error parsing JSON: ${err}`);
         }
+      });
+    });
+
+    request.on('error', (err) => {
+      reject(`Error making HTTP request: ${err}`);
+    });
+
+    request.end();
+  });
+};
+
+if (require.main === module) {
+  getTodoTitle()
+    .then(title => console.log(title))
+    .catch(err => {
+      console.error(err);
+      process.exit(1);
+    });
+} else {
+  module.exports = getTodoTitle;
+}
+
+// Create HTTP server using `http` module
+let server = http.createServer((req, res) => {
+  if (req.url === '/todo') {
+    if (req.method === 'GET') {
+      getTodoTitle()
+        .then(title => {
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end(title);
+        })
+        .catch(err => {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Error fetching data');
+        });
     } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('404 Not Found');
+      res.writeHead(405, { 'Content-Type': 'text/plain' });
+      res.end('Method Not Allowed');
     }
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
 });
 
 server.listen(3000, () => {
-    console.log('Server connected');
+  console.log('Server connected');
 });
